ad6c98c121e0bf903b4e842fc337b5ad71b8b8fd
tenongene <thierryss@gmail.com>
2022-11-15 22:42:39 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,12 +139,10 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
 
 ## List test
     def test_get_account_list(self):
@@ -213,3 +211,4 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+        
